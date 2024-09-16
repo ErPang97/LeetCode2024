a07fe82d1f39bb734535ea3b0b1bb1b7b592d4b4
Ryan Lim Pangilinan <75344885+ErPang97@users.noreply.github.com>
2024-09-16 17:21:52 -0400

    Time: 0 ms (100.00%) | Memory: 42.1 MB (37.92%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,79 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // P:
+        // E:
+        // D: none 
+        // A:
+            /**
+            - general idea, use the beginning and end indices
+            to help narrow down which row, the target should be 
+            found in
+            - then, once thats known, do a binary search on 
+            the specified row
+            - pseucode:
+                - use minRow and maxRow, which will be firstRow initially, 
+                and maxRow 
+                - get midPoint row = (lastRow + firstRow)/2
+                - if( maxRow lastInt > target > maxRow firstInt) 
+                    - we know val must be in last row,
+                    search this row
+                - if( minRow lastInt > target > minRow firstInt)
+                    - val is in min row, 
+                    so search this row
+                - if(target > midRow firstInt && target > midRow lastInt
+                target < maxRow firstInt) 
+                    - update minRow to be midRow
+                - if(target > minRow firstInt && target < midRow firstInt)
+                    - update maxRow to be midRow
+                - if(target < midRow || target > maxRow)
+                    - target must not be in matrix 
+             */
+        // C: 
+
+        int minRowIndex = 0;
+        int maxRowIndex = matrix.length - 1;
+
+        int width = matrix[0].length;
+
+        int targetRowIndex = 0;
+
+        while(minRowIndex <= maxRowIndex){
+
+            int midRowIndex = (minRowIndex + maxRowIndex)/2;
+            if(matrix[midRowIndex][0] <= target 
+                    && matrix[midRowIndex][width - 1] >= target) {
+                // we know its on the mid row
+                targetRowIndex = midRowIndex;
+                break;
+            }
+            else if(target > matrix[midRowIndex][0]
+                    && target > matrix[midRowIndex][width - 1]){
+                // its not on mid row and greater, so update min, to midRow
+                minRowIndex = midRowIndex + 1;
+            }
+            else if(target < matrix[midRowIndex][0]
+                    && target < matrix[midRowIndex][width - 1]){
+                // its not on mid row and less, so update max, to midRow
+                maxRowIndex = midRowIndex - 1;
+            }
+        }
+
+        int[] searchRow = matrix[targetRowIndex];
+
+        int min = 0;
+        int max = width-1;
+        while(min <= max){
+
+            int mid = (min + max)/2;
+            if(target == searchRow[mid]){
+                return true;
+            } else if(target > searchRow[mid]){
+                min = mid + 1;
+            } else if(target < searchRow[mid]) {
+                max = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+}
