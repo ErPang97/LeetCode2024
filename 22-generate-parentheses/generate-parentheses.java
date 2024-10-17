cf672515c5cac91a23028ed520ad51efffde12ec
Ryan Lim Pangilinan <75344885+ErPang97@users.noreply.github.com>
2024-10-17 11:30:45 -0400

    Time: 3 ms (14.67%) | Memory: 43.1 MB (62.26%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,54 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        // P: - We know: there will always be n of "(" and ")"
+        //      so we just need to figure out the different schemes
+        //      that are valid.
+        // E: 
+        // D: - stack
+        // A: - backtracking problem?
+        /*   - general idea is to build up the solution 
+         *   - keep count of how many open parantheses
+         *   and closed parantheses 
+         *   - build recursively until number of closed
+         *   and open = 3
+         *       - if numClosed and numOpen == n
+         *            - build solution and return it
+         *       - if numOpen < 3
+         *            - add "(" to stack
+         *            - backtrack and recursve with numOpen + 1
+         *            - pop stack 
+         *       - if numClosed < numOpen:
+         *            - add ")"
+         *            - backtrack/recurse with numClosed + 1
+         */
+        // C:
+
+        Stack<String> stack = new Stack<>();
+        List<String> result = new ArrayList<>();
+        result = backTrack(n, 0, 0, 
+                stack, result);
+        return result;
+    }
+
+    public List<String> backTrack(int n, int numOpen, int numClosed, 
+            Stack<String> stack, List<String> result) {
+
+        if(numOpen == numClosed && numOpen == n && numClosed == n) {
+            result.add(String.join("", stack));
+        }
+
+        if(numOpen < n){
+            stack.add("(");
+            backTrack(n, numOpen+1, numClosed, stack, result);
+            stack.pop();
+        }
+
+        if(numClosed < numOpen) {
+            stack.add(")");
+            backTrack(n, numOpen, numClosed+1, stack, result);
+            stack.pop();
+        }
+
+        return result;
+    }
+}
