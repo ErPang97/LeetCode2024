899cd6381d7b0785a281420e49edab9cf22c262f
Ryan Lim Pangilinan <75344885+ErPang97@users.noreply.github.com>
2025-02-28 15:22:30 -0500

    Time: 33 ms (98.81%) | Memory: 53.6 MB (9.24%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,7 +4,8 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    var filtered = [];
+    var filtered = []; // var has scope across the function body
+                       // while let is only within the immediate opening/closing blocks
     for (var i = 0; i < arr.length; i++) {
         if (fn(arr[i], i))
             filtered.push(arr[i])
