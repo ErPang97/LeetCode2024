75b6adc4de3ee6a6cfb7a3635f716fd244ece8cb
Ryan Lim Pangilinan <75344885+ErPang97@users.noreply.github.com>
2026-03-10 23:44:22 -0400

    Time: 15 ms (22.14%) | Memory: 52.3 MB (96.77%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,32 +1,45 @@
 class Solution {
+    /**
+    P:
+        - given an int[] nums
+        - return the majority element
+            - this appears more than floor(n/2) times
+    E:
+        - the examples make sense:
+            ie: [3, 2, 3]
+                - 3 appears twice while 2 only appears once, so 3 is returned
+            ie: [2, 2, 1, 1, 1, 2, 2]
+                - 2 appears 4 times, and 1 appears 3 times, so 2 is returned
+    D:
+        - an O(n) space solution would use a HashMap to map the counts of each
+    A:
+        - an O(n) space solution, as stated, would use a map
+        - we would iterate through the array and update the counts
+        - however, can we accomplish this without using extra space?
+            - we may be able if we sorted?
+            - but without sorting which takes n log n?
+    C:
+     */
     public int majorityElement(int[] nums) {
-        // P:
-        // E:
-        // D: - HashMap for O(n) space solution
-        // A:
-        /**
-        - the easiest method would just be to 
-        map each number in nums to a counts HashMAp
-        and find the number that returns the largest key
-         */
-        // C: 
-
-        HashMap<Integer, Integer> counts = new HashMap<>();
-        for(int i: nums){
-            if(counts.get(i) != null){
-                counts.replace(i, counts.get(i)+1);
+        // Brute Force Algo:
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
             } else {
-                counts.put(i, 1);
+                map.put(num, 1);
             }
         }
 
-        int majority = nums[0];
-        for(int i: nums) {
-            if(counts.get(i) > counts.get(majority)){
-                majority = i;
+        int max = Integer.MIN_VALUE;
+        int maxCount = 0;
+        for (int key : map.keySet()) {
+            int currentCount = map.get(key);
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+                max = key;
             }
         }
-
-        return majority;
+        return max;
     }
 }
